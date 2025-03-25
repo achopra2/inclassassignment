767f3595f4e522bf5d7adffd54d50a3788082650
achopra2 <achopra2@pride.hofstra.edu>
2025-03-24 23:47:03 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Display a message in the browser console
+console.log("CSS + JavaScript is powerful!");
+
+// Alert message when the page loads
+window.onload = function () {
+    alert("Welcome to Assignment 5 with JavaScript!");
+};
+
+// light mode and dark mode
+function changeTheme() {
+    document.body.classList.toggle("dark-mode");
+}
